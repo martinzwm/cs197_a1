3fd755899d50608f7850cd2fdda7dba3f4dca586
Martin Ma <z74ma@uwaterloo.ca>
2022-09-14 14:58:43 -0400

    Add docstring

--- merge_k_sorted_lists.py
+++ merge_k_sorted_lists.py
@@ -1,10 +1,17 @@
 class ListNode:
+    """
+    Class for nodes in linked list, which is I/O for mergeKLists().
+    """
     def __init__(self, val, next=None):
         self.val = val
         self.next = next
 
 
 def mergeKLists(lists):
+    """
+    Sort a list of linked list by storing their values in a array, sort the array and reconstruct a sorted linked list.
+    Note this may not be the best implementation b/c we didn't use the property that each linked list in the input is sorted.
+    """
     # stores the values of the nodes in a list
     node_vals = []
     for curr in lists:
@@ -23,6 +30,9 @@ def mergeKLists(lists):
 
 
 def createKLists(arrays):
+    """
+    Utility function to create lists of linked list from list of arrays, used for creating input for mergeKLists().
+    """
     lists = []
     for array in arrays:
         head = curr = ListNode(0)
@@ -34,6 +44,9 @@ def createKLists(arrays):
 
 
 def readList(list):
+    """
+    Utitlity function to create array from a linked list, used for testing the output of mergeKLists().
+    """
     nums = []
     curr = list
     while curr:
@@ -43,11 +56,18 @@ def readList(list):
 
 
 def test(arrays):
+    """
+    Utitlity function to test the correctness of mergeKLists().
+    """
     lists = createKLists(arrays)
     list = mergeKLists(lists)
     array = readList(list)
     return array
 
+
 if __name__ == "__main__":
+    """
+    Contain test cases for mergeKLists().
+    """
     arrays = [[1,4,5],[1,3,4],[2,6]]
     print(test(arrays))

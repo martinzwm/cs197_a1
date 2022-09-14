21278564dac5ca8d4844ea9aee5bf1be8bed958c
Martin Ma <z74ma@uwaterloo.ca>
2022-09-14 14:32:44 -0400

    Add first attempt
    Incorporate a brute force solution to gain intuition

--- merge_k_sorted_lists.py
+++ merge_k_sorted_lists.py
@@ -4,4 +4,18 @@ class ListNode:
         self.next = next
 
 def mergeKLists(lists):
-    pass
+    # stores the values of the nodes in a list
+    node_vals = []
+    for curr in lists:
+        while curr:
+            node_vals.append(curr.val)
+            curr = curr.next
+    
+    # construct a new linked list based on the sorted list
+    node_vals.sort()
+    head = curr = ListNode()
+    for val in node_vals:
+        curr.next = ListNode(val)
+        curr = curr.next
+        
+    return head.next
